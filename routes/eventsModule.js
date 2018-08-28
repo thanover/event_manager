b0cbc75f83cec1054e5926ac2cc2e384c7cd5b81
Tom Hanover <thomas.hanover@gmail.com>
2018-08-27 22:39:09 -0400

    added multiple invitees array

--- routes/eventsModule.js
+++ routes/eventsModule.js
@@ -1,22 +1,22 @@
 const Event = require('../models/event').eventModel;
 const User = require('../models/user').userModel;
 
-const displayEvents = (req, res, next) => {    
+const displayEvents = (req, res, next) => {
     Event.find()
-    .populate('Owner')
-    .exec((err, events) => {
-        if(err){
-            console.log(err)
-        }
-        res.render(
-            'events', 
-            {
-            title: 'events',
-            events
+        .populate('Owner')
+        .exec((err, events) => {
+            if (err) {
+                console.log(err)
             }
-        );
+            res.render(
+                'events',
+                {
+                    title: 'events',
+                    events
+                }
+            );
 
-    });
+        });
 };
 
 const addEvent = (req, res, next) => {
@@ -26,13 +26,13 @@ const addEvent = (req, res, next) => {
         if (!users)
             return res.render('404');
 
-            res.render(
-                'add_event', 
-                {
-                    users
-                }
-            );
-    });   
+        res.render(
+            'add_event',
+            {
+                users
+            }
+        );
+    });
 }
 
 const saveEvent = (req, res, next) => {
@@ -41,9 +41,15 @@ const saveEvent = (req, res, next) => {
     event.Date = req.body.Date;
     event.Description = req.body.Description;
     event.Owner = global.user._id;
+    console.trace(req.body.invitees);
+    let invitees = req.body.invitees;
 
-    event.save( (err) => {
-        if(err){
+    invitees.forEach(invitee => {
+        event.Invites.invited.push(invitee);
+    });
+
+    event.save((err) => {
+        if (err) {
             console.log(err);
             return;
         } else {
@@ -62,7 +68,7 @@ const deleteEvent = (req, res, next) => {
             return res.render('404');
 
 
-            event.remove((err) => {
+        event.remove((err) => {
             if (err)
                 console.log("Error deleting : %s ", err);
             res.redirect('/events');
@@ -74,19 +80,30 @@ const displayEventDetails = (req, res, next) => {
     let id = req.params.id;
 
     Event.findById(id)
-    .populate('Owner')
-    .exec((err, event) => {
-        if (err)
-            console.log("Error Selecting : %s ", err);
-        if (!event)
-            return res.render('404');
-        // console.trace(event);
-        res.render('event_details',
-            {
-                event
-            }
-        )
-    });
+        .populate('Owner')
+        .exec((err, event) => {
+            var tmpEvent = event.toObject();
+            var tmpInvitees = [];
+            var invitees = tmpEvent.Invites.invited;
+            console.trace(invitees);
+            invitees.forEach(invitee => {
+                User.findById(invitee, (err, invitee) => {
+                    tmpInvitees.push(invitee);
+                });
+            });
+            if (err)
+                console.log("Error Selecting : %s ", err);
+            if (!event)
+                return res.render('404');
+            // console.trace(event);
+            res.render('event_details',
+                {
+                    event,
+                    invitees: tmpInvitees
+                }
+            );
+
+        });
 };
 
 module.exports = {
